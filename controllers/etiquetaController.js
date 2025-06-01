60146efb0b1b1e5e875a3d68d7b4302e933a35df
SamFZero <samuelfalla12@gmail.com>
2025-06-01 16:44:57 -0500

    new

--- controllers/etiquetaController.js
+++ controllers/etiquetaController.js
@@ -17,3 +17,37 @@ exports.obtenerEtiquetasPorUsuario = async (req, res) => {
     const etiquetas = await Etiqueta.find({ usuario_id: req.usuario.id }).populate("persona_id");
     res.json(etiquetas);
 };
+
+exports.famososMasEtiquetados = async (req, res) => {
+    try {
+        const resultado = await Etiqueta.aggregate([
+            {
+                $group: {
+                    _id: "$persona_id",
+                    cantidad: { $sum: 1 },
+                },
+            },
+            {
+                $lookup: {
+                    from: "personas",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "persona",
+                },
+            },
+            { $unwind: "$persona" },
+            {
+                $project: {
+                    nombre: "$persona.nombre",
+                    tipo: "$persona.tipo",
+                    cantidad: 1,
+                },
+            },
+            { $sort: { cantidad: -1 } },
+            { $limit: 10 },
+        ]);
+        res.json(resultado);
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+};
--- routes/etiquetaRoutes.js
+++ routes/etiquetaRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { crearEtiqueta, obtenerEtiquetasPorUsuario } = require("../controllers/etiquetaController");
+const { crearEtiqueta, obtenerEtiquetasPorUsuario, famososMasEtiquetados } = require("../controllers/etiquetaController");
 const { auth } = require("../middleware/authMiddleware");
 
 router.post("/", auth, crearEtiqueta);
 router.get("/mias", auth, obtenerEtiquetasPorUsuario);
+router.get("/top", auth, famososMasEtiquetados);
 
 module.exports = router;
